f7246a0ff1cc14bb396368344ff1b1c6c53878a6
Buben044 <118118291+Buben044@users.noreply.github.com>
2023-05-20 14:46:29 +0300

    Update main.cpp

--- lab13_2.0/main.cpp
+++ lab13_2.0/main.cpp
@@ -1,141 +1,147 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
+template<typename T>
 class Matrix {
 private:
     int rows;
     int cols;
-    std::vector<std::vector<double>> data;
+    std::vector<std::vector<T>> data;
 
 public:
-    // Конструкторы
-    Matrix() : rows(0), cols(0) {}
-    Matrix(int rows, int cols) : rows(rows), cols(cols), data(rows, std::vector<double>(cols, 0.0)) {}
-    Matrix(int rows, int cols, const std::vector<double>& data) : rows(rows), cols(cols), data(rows, std::vector<double>(cols, 0.0)) {
+    Matrix<T>() : rows(0), cols(0) {}
+    Matrix<T>(int rows, int cols) : rows(rows), cols(cols), data(rows, std::vector<T>(cols, 0.0)) {}
+    Matrix<T>(int rows, int cols, const std::vector<T>& data) : rows(rows), cols(cols), data(rows, std::vector<T>(cols, 0.0)) {
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 this->data[i][j] = data[i * cols + j];
             }
         }
     }
-
-
-
-    // Методы создания матриц
-    static Matrix zeros(int rows, int cols) {
-        return Matrix(rows, cols); ч
+    
+    void insert(T n, int i, int j) {
+        if (rows < i + 1) {
+            data.resize(rows + 1);
+            rows++;
+        }if (data[i].size() < j + 1) data[i].push_back(n);
+        else data[i][j] = n;
+        if (cols < j + 1) cols++;
     }
-
-    static Matrix ones(int size) {
-        Matrix mat(size, size);
+    
+    static Matrix<T> zeros(int rows, int cols) {
+        return Matrix<T>(rows, cols);
+    }
+    
+    static Matrix<T> ones(int size) {
+        Matrix<T> mat(size, size);
         for (int i = 0; i < size; ++i) {
-            mat(i, i) = 1.0;
+            mat(i, i) = (T) 1;
         }
         return mat;
-    }
-
-    // Методы доступа к элементам матрицы
-    double& operator()(int row, int col) {
+    
+    
+    }T& operator()(int row, int col) {
         return data[row][col];
     }
-
-    const double& operator()(int row, int col) const {
+    
+    const T& operator()(int row, int col) const {
         return data[row][col];
     }
-
-    // Операторы сравнения
-    bool operator==(const Matrix& other) const {
+    
+    bool operator==(const Matrix<T>& other) const {
         if (rows != other.rows || cols != other.cols) {
             return false;
-        }
-        for (int i = 0; i < rows; ++i) {
+        }for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 if (data[i][j] != other.data[i][j]) {
                     return false;
                 }
             }
-        }
-        return true;
+        }return true;
     }
-
-    bool operator!=(const Matrix& other) const {
+    bool operator!=(const Matrix<T>& other) const {
         return !(*this == other);
     }
-
-    // Операторы математических операций
-    Matrix operator+(const Matrix& other) const {
-        Matrix result(rows, cols);
+    
+    Matrix<T> operator+(const Matrix<T>& other) const {
+        if (!(this->is_same_size_as(other))) {
+            std::cerr << "Matrices are not of the same size" << std::endl;
+            return *this;
+        }
+        Matrix<T> result(rows, cols);
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
-                result(i, j) = data[i][j] + other(i, j);
+                result(i, j) = data[i][j] +  (T) other(i, j);
             }
-        }
-        return result;
+        }return result;
     }
-
-    Matrix operator-(const Matrix& other) const {
-        Matrix result(rows, cols);
+    
+    Matrix<T> operator-(const Matrix<T>& other) const {
+        if (!(this->is_same_size_as(other))) {
+            std::cerr << "Matrices are not of the same size" << std::endl;
+            return *this;
+        }Matrix<T> result(rows, cols);
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
-                result(i, j) = data[i][j] - other(i, j);
+                result(i, j) = data[i][j] - (T) other(i, j);
             }
-        }
-        return result;
+        }return result;
     }
-
-    Matrix operator*(double scalar) const {
-        Matrix result(rows, cols);
+    
+    Matrix<T> operator*(double scalar) const {
+        Matrix<T> result(rows, cols);
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
-                result(i, j) = data[i][j] * scalar;
+                result(i, j) = (double)data[i][j] * scalar;
             }
-        }
-        return result;
+        }return result;
     }
-
-    friend Matrix operator*(double scalar, const Matrix& mat) {
+    
+    friend Matrix<T> operator*(double scalar, const Matrix<T>& mat) {
         return mat * scalar;
-    }
 
-    Matrix operator*(const Matrix& other) const {
-        Matrix result(rows, cols);
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < other.cols; ++j) {
-                double sum = 0.0;
-                for (int k = 0; k < cols; ++k) {
-                    sum += data[i][k] * other(k, j);
-                }
-                result(i, j) = sum;
-            }
+    }
+    
+    Matrix<T> operator*(const Matrix<T>& other) const {
+        if (!(this->is_multiplicable_with(other))) {
+            std::cerr << "Matrices are not compatible for multiplication" << std::endl;
+            return *this;
         }
-        return result;
+        Matrix<T> result(rows, other.cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int k = 0; k < other.cols; ++k) {
+                T sum = 0;
+                for (int j = 0; j < cols; ++j) {
+                    sum += data[i][j] * other(j, k);
+                }
+                result(i, k) = sum;
+            }
+        }return result;
     }
-
-    Matrix& operator=(const Matrix& other) {
+    
+    Matrix<T>& operator=(const Matrix<T>& other) {
         if (this != &other) {
             rows = other.rows;
             cols = other.cols;
             data = other.data;
-        }
-        return *this;
+        }return *this;
     }
-
-    // Методы ввода-вывода матриц
-    friend std::istream& operator>>(std::istream& in, Matrix& mat) {
+    
+    friend std::istream& operator>>(std::istream& in, Matrix<T>& mat) {
         int rows, cols;
         in >> rows >> cols;
-        mat = Matrix(rows, cols);
+        mat = Matrix<T>(rows, cols);
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 in >> mat(i, j);
             }
-        }
-        return in;
+        }return in;
     }
-
-    friend std::ostream& operator<<(std::ostream& out, const Matrix& mat) {
-        out << mat.rows << "<-rows cols->" << mat.cols << std::endl << std::endl;
+    
+    friend std::ostream& operator<<(std::ostream& out, const Matrix<T>& mat) {
+        out << mat.rows << " " << mat.cols << std::endl << std::endl;
         for (int i = 0; i < mat.rows; ++i) {
             for (int j = 0; j < mat.cols; ++j) {
                 out << mat(i, j) << " ";
@@ -144,31 +150,104 @@ public:
         }
         return out;
     }
-
-    // Методы чтения-записи матриц в файл
+    
     void read(const std::string& filename) {
         std::ifstream infile(filename);
         infile >> *this;
     }
-
+    
     void write(const std::string& filename) const {
         std::ofstream outfile(filename);
         outfile << *this;
     }
-    //проверка на умножение
-    bool is_multiplicable_with(const Matrix& other) const {
+    
+    bool is_multiplicable_with(const Matrix<T>& other) const {
         return cols == other.rows;
     }
-    //проверка на равенство размеров матрицы
-    bool is_same_size_as(const Matrix& other) const {
+    
+    bool is_same_size_as(const Matrix<T>& other) const {
         return (rows == other.rows) && (cols == other.cols);
+    }
+    
+    double det() {
+        if (rows != cols) {
+            std::cout << "The Matrix<T> must be square" << std::endl << std::endl;
+            return 0;
+        }if (rows == 1) return (double)data[0][0];
+        if (rows == 2) return data[0][0] * data[1][1] - data[0][1] * data[1][0];
+        double res = 0;
+        for (int i = 0; i < rows; i++) {
+            double temp = 1;
+            for (int j = 0; j < rows; j++) {
+                temp *= (double)data[j][(i + j) % rows];
+            }res += temp;
+        }for (int i = 0; i < rows; i++) {
+            double temp = 1;
+            for (int j = 0; j < rows; j++) {
+                temp *= (double)data[j][(rows + i - j) % rows];
+            }res -= temp;
+        }return res;
+    }
+    
+    Matrix<T> get_small(int i, int j) {
+        Matrix<T> m;
+        bool flag = false;
+        for (int k = 0; k < rows; k++) {
+            if (k != i) {
+                bool flag1 = false;
+                for (int g = 0; g < cols; g++) {
+                    if (g != j) {
+                        int k1, g1;
+                        if (flag) k1 = k - 1;
+                        else k1 = k;
+                        if (flag1) g1 = g - 1;
+                        else g1 = g;
+                        m.insert(data[k][g], k1, g1);
+                    }
+                    else flag1 = true;
+                }
+            }
+            else flag = true;
+        }return m;
+    }
+    
+    Matrix<T> transpon() {
+        Matrix<T> m;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                m.insert(data[i][j], j, i);
+            }
+        }return m;
+    }
+    
+    Matrix<T> findAlgebraicComplement() {
+        if (rows != cols) {
+            std::cout << "The Matrix<T> must be square" << std::endl << std::endl;
+            return *this;
+        }Matrix<T> m;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                m.insert(this->get_small(i, j).det() * pow((-1), i + j), i, j);
+            }
+        }return m;
+    }
+    
+    Matrix<T> operator!() {
+        if (!(this->det())) {
+            std::cout << "The Matrix<T> must be square" << std::endl << std::endl;
+            return *this;
+        }if (this->is_E()) return *this;
+        if (this->is_O()) return *this;
+        Matrix<T> m = (this->findAlgebraicComplement().transpon() / this->det());
+        return m;
     }
 };
 
 int main() {
-    Matrix mat1(2, 3, { 1, 2, 3, 4,5,6 });
-    Matrix mat2 = Matrix::ones(2);
-    Matrix mat3;
+
+    Matrix<double> mat1(2, 3, { 1, 2, 3, 4,5,6 });
+    Matrix<double> mat2 = Matrix<double>::ones(2);
+    Matrix<double> mat3;
 
     std::cin >> mat3;
 
@@ -177,43 +256,32 @@ int main() {
     std::cout << "mat3: " << std::endl << mat3 << std::endl;
 
 
+    Matrix<double> mat4 = mat1 * mat2;
+    std::cout << "mat4 = mat1 * mat2: " << std::endl << mat4 << std::endl;
 
-    if (mat1.is_multiplicable_with(mat2)) {
-        Matrix mat4 = mat1 * mat2;
-        std::cout << "mat4 = mat1 * mat2: " << std::endl << mat4 << std::endl;
-    }
-    else {
-        std::cerr << "Matrices are not compatible for multiplication" << std::endl << std::endl;
-    }
+    Matrix<double> mat5 = mat1 + mat3;
+    std::cout << "mat6 = mat1 + mat3: " << std::endl << mat5 << std::endl;
 
+    Matrix<double> mat6 = mat2 - mat3;
+    std::cout << "mat6 = mat2 - mat3: " << std::endl << mat6 << std::endl;
 
-    if (mat1.is_same_size_as(mat3)) {
-        Matrix mat5 = mat1 + mat3;
-        std::cout << "mat6 = mat1 + mat3: " << std::endl << mat5 << std::endl;
-    }
-    else {
-        std::cerr << "Matrices are not of the same size" << std::endl << std::endl;
-    }
-
-
-
-    if (mat2.is_same_size_as(mat3)) {
-        Matrix mat6 = mat2 - mat3;
-        std::cout << "mat6 = mat2 - mat3: " << std::endl << mat6 << std::endl;
-    }
-    else {
-        std::cerr << "Matrices are not of the same size" << std::endl << std::endl;
-    }
-
-
-    Matrix mat7 = mat1 * 2;
+    
+    Matrix<double> mat7 = mat1 * 2.0;
     std::cout << "mat7 = mat1 * 2: " << std::endl << mat7 << std::endl;
 
 
-    mat3.write("matrix.txt");
-    Matrix mat8;
-    mat8.read("matrix.txt");
+    mat3.write("Matrix.txt");
+    Matrix<int> mat8;
+
+    mat8.read("Matrix.txt");
     std::cout << "mat8 (from file): " << std::endl << mat8 << std::endl;
+
+    Matrix<double> mat10 = mat1;
+    mat10 = mat10.transpon();
+
+    Matrix<double> mat9 = mat10 * mat1;
+    std::cout << "mat9 = mat1 * mat10: " << std::endl << mat9 << std::endl;
+
 
     return 0;
 }
